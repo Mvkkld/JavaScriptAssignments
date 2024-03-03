c4fcf8da8c19f935af0e4fcb9c625409ace1fc1d
Mvkkld <mvk40kld@gmail.com>
2024-03-03 22:37:09 +0500

    chapter1-4-assignment

--- chapter_3/app.js
+++ chapter_3/app.js
@@ -5,10 +5,10 @@ var visitTrack= "You have visited this site " + 14 + " times"
 alert(visitTrack)
 
 var birthYear=1990
-document.write("My Birth Year Is " , birthYear, ' Data type of my variable is number')
+document.write("My Birth Year Is " , birthYear ,"<br>"+' Data type of my variable is number'+'<br>')
 
 var visitorName="Ali"
 var item="TShirt"
 var qty=2
-var website="(www.xyzClothing.com)"
-document.write(visitorName + 'Ordered' + qty + item + 'on' + website + 'Clothing store')
+var website="www.xyzClothing.com"
+document.write("<b>"+visitorName +"</b>", ' Ordered ' ,"<b>"+ qty ,' ', item ,"</b>"+ ' on ' , website , ' Clothing store ')
