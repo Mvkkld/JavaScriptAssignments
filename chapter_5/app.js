b6b5fd5b930ca621c12308487f08c13c6d34f60e
Mvkkld <mvk40kld@gmail.com>
2024-03-04 00:49:35 +0500

    chapter_5-is-statr

---
+++ chapter_5/app.js
@@ -0,0 +1,67 @@
+// 1.
+
+var num1=3
+var num2=5
+var result=num1+num2
+
+document.write("<h3>Sum Of ",num1," and ",num2," is ",result)
+
+var num1=3
+var num2=5
+var result=num1-num2
+
+document.write("<h3>Substract Of ",num1," and ",num2," is ",result)
+
+var num1=3
+var num2=5
+var result=num1*num2
+
+document.write("<h3>Multiply Of ",num1," and ",num2," is ",result)
+
+var num1=3
+var num2=5
+var result=num1/num2
+
+document.write("<h3>Division Of ",num1," and ",num2," is ",result)
+
+var num1=3
+var num2=5
+var result=num1%num2
+
+document.write("<h3>Modulus Of ",num1," and ",num2," is ",result)
+
+// 2.
+
+var var1
+var var2=5
+document.write("<h3>Value after variable declaration is: ",var1)
+document.write("<h3>Initial Value: ",var2)
+var var2incr=++var2
+document.write("<h3>Value after increment is: ",var2incr)
+var var2incr=var2+7
+
+document.write("<h3>Value after addition is: ",var2incr)
+var var2incr=var2incr-1
+document.write("<h3>Value after decrement is: ",var2incr)
+
+var var2incr=var2incr%6
+document.write("<h3>The remindar is: ",var2incr)
+
+var ticketPrice=600
+var persons=5
+var bill=ticketPrice*persons
+
+document.write("<h3>Total cost to buy ",persons," tickets to a movie is: ",bill,"PKR")
+
+var table=4
+document.write("<h1> Table of 4")
+document.write('<h2>',table,' X ',1,' = ',table*1)
+document.write('<h2>',table,' X ',2,' = ',table*2)
+document.write('<h2>',table,' X ',3,' = ',table*3)
+document.write('<h2>',table,' X ',4,' = ',table*4)
+document.write('<h2>',table,' X ',5,' = ',table*5)
+document.write('<h2>',table,' X ',6,' = ',table*6)
+document.write('<h2>',table,' X ',7,' = ',table*7)
+document.write('<h2>',table,' X ',8,' = ',table*8)
+document.write('<h2>',table,' X ',9,' = ',table*9)
+document.write('<h2>',table,' X ',10,' = ',table*10)
